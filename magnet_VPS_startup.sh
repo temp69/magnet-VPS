00bbff0a19385be7b94d7ce5ab39d63a42ddf165
TSC <36497576+temp69@users.noreply.github.com>
2018-12-28 13:36:55 +0100

    Update for new wallet

--- magnet_VPS_startup.sh
+++ magnet_VPS_startup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-WALLET_URL="https://magnetwork.io/Wallets/magnet-qt-1804.tar.gz"
-WALLET_ARCH="magnet-qt-1804.tar.gz"
-BOOTSTRAP_URL="https://magnetwork.io/Wallets/bootstrap.zip"
-BOOTSTRAP_FILE="bootstrap.zip"
+WALLET_URL=$(curl -s https://api.github.com/repos/magnetwork/mag/releases/latest | grep browser_download_url | grep -e "x86_64-linux" | cut -d '"' -f 4)
+WALLET_ARCH=$(echo $WALLET_URL | cut -d '/' -f 9)
+BOOTSTRAP_URL=$(curl -s https://api.github.com/repos/magnetwork/mag/releases/latest | grep browser_download_url | grep -e "bootstrap" | cut -d '"' -f 4)
+BOOTSTRAP_FILE=$(echo $BOOTSTRAP_URL | cut -d '/' -f 9)
 WALLET_DESKTOP="/root/Desktop"
 WALLET_DIRECTORY="/root/magnet"
-WALLET_DATADIR="/root/.magnet"
+WALLET_DATADIR="/root/.mag"
+WALLT_CONFIGFILE="mag.conf"
+MAGNET_PRESSPACKAGE_URL="https://magnetwork.io/home/resources/MAG_PressPackage.zip"
+MAGNET_PRESSPACKAGE="MAG_PressPackage.zip"
 
-function prepare_swap() {
+function prepareSwap() {
 	if free | awk '/^Swap:/ {exit !$2}'; then
 		printf "Swap exists\n" > /dev/tty1
 	else
@@ -16,37 +19,36 @@ function prepare_swap() {
 		mkswap /swapfile
 		swapon /swapfile
 		echo "/swapfile none swap sw 0 0" >> /etc/fstab
-		echo 'vm.swappiness=10' | tee -a /etc/sysctl.conf
-		echo 'vm.vfs_cache_pressure=50' | tee -a /etc/sysctl.conf
 		printf "Swap with 2GB created\n" > /dev/tty1
 	fi
 }
 
-function update_system() {
+function updateSystem() {
 	sudo apt-get -y update
 	sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
 }
 
-function install_prerequisits() {
+function installPrerequisits() {
+	apt-get -y update
 	apt-get -yq install lubuntu-core
 	apt-get -yq install lxterminal
 	apt-get -yq install xarchiver
-	apt-get -yq install libboost-all-dev
-	apt-get -yq install qtbase5-dev
+	#apt-get -yq install libboost-all-dev
+	#apt-get -yq install qtbase5-dev
 	apt-get -yq install libminiupnpc-dev
 	apt-get -yq install firefox
 	apt-get -yq install gedit
 	apt-get -yq install onboard
 	apt-get -yq install pwgen
 	apt-get -yq install unzip
-	apt-get -yq install software-properties-common
-	apt-get -yq install python-software-properties	
-	add-apt-repository -y ppa:bitcoin/bitcoin
-	apt-get -y update
-	apt-get -yq install libdb4.8-dev libdb4.8++-dev
+	#apt-get -yq install software-properties-common
+	#apt-get -yq install python-software-properties	
+	#add-apt-repository -y ppa:bitcoin/bitcoin
+	#apt-get -y update
+	#apt-get -yq install libdb4.8-dev libdb4.8++-dev
 }
 
-function xorg_config() {
+function xorgConfig() {
 	touch /usr/share/X11/xorg.conf.d/xorg.conf
 	cat > /usr/share/X11/xorg.conf.d/xorg.conf << EOL
 	Section "Device"
@@ -73,7 +75,7 @@ function xorg_config() {
 EOL
 }
 
-function lightdm_config() {
+function lightdmConfig() {
 	touch /etc/lightdm/lightdm.conf
 	cat > /etc/lightdm/lightdm.conf << EOL
 [SeatDefaults]
@@ -85,76 +87,63 @@ EOL
 echo 'keyboard=onboard' >> /etc/lightdm/lightdm-gtk-greeter.conf.d/30_lubuntu.conf
 }
 
-function download_magnet_wallet() {
+function downloadMagnetWallet() {
 	mkdir -p $WALLET_DESKTOP
 	mkdir -p $WALLET_DIRECTORY
 	mkdir -p $WALLET_DATADIR
 	cd $WALLET_DIRECTORY
+	rm -rfv *
 	wget $WALLET_URL
 	tar -xvzf $WALLET_ARCH
-	cp $WALLET_DIRECTORY/magnet.desktop $WALLET_DESKTOP
+	MAGQTWALLET=$(find -name mag-qt)
+	cp $MAGQTWALLET $WALLET_DIRECTORY
+	wget $MAGNET_PRESSPACKAGE_URL
+	unzip $MAGNET_PRESSPACKAGE
+	MAGICON=$(find -name MAG_logo.svg)
+	cp $MAGICON $WALLET_DIRECTORY
 }
 
-function config_magnet_wallet() {
+function createMagnetDesktopIcon() {
+	mkdir -p $WALLET_DESKTOP
+	cd $WALLET_DESKTOP
+	touch magnet.Desktop
+	cat > magnet.Desktop << EOL
+[Desktop Entry]
+Name=MAGNET QT WALLET
+Comment=Launches the magnet qt wallet
+Exec=/root/magnet/mag-qt
+Terminal=false
+Type=Application
+Icon=/root/magnet/MAG_logo.svg
+EOL
+}
+
+function configMagnetWallet() {
 	mkdir -p $WALLET_DATADIR
 	cd $WALLET_DATADIR
 	wget $BOOTSTRAP_URL
 	unzip $BOOTSTRAP_FILE
 	RANDOM_RPC_USER=$(pwgen 8 1)
 	RANDOM_RPC_PASS=$(pwgen 20 1)
-	touch $WALLET_DATADIR/magnet.conf
-	cat > $WALLET_DATADIR/magnet.conf << EOL
+	touch $WALLET_DATADIR/$WALLT_CONFIGFILE
+	cat > $WALLET_DATADIR/$WALLT_CONFIGFILE << EOL
 rpcallowip=127.0.0.1
-rpcport=17179
+rpcport=17103
 rpcuser=$RANDOM_RPC_USER
 rpcpassword=$RANDOM_RPC_PASS
 server=1
 daemon=1
 listen=1
-staking=0
-port=17177
+staking=1
+port=17172
 debug=all
-addnode=45.76.81.227:17177
-addnode=104.207.151.96:17177
-addnode=45.32.140.188:17177
-addnode=108.61.209.211:17177
-addnode=107.191.62.63:17177
-addnode=45.32.48.202:17177
-addnode=108.61.176.156:17177
-addnode=45.77.7.202:17177
-addnode=45.76.130.131:17177
-addnode=45.77.138.103:17177
-addnode=104.238.188.10:17177
-addnode=45.77.232.192:17177
-addnode=209.250.239.217:17177
-addnode=209.222.30.95:17177
-addnode=104.238.165.132:17177
-addnode=108.61.208.135:17177
-addnode=144.202.71.114:17177
-addnode=45.63.42.106:17177
-addnode=209.250.242.7:17177
-addnode=207.246.124.100:17177
-addnode=45.76.83.24:17177
-addnode=207.246.117.153:17177
-addnode=45.77.163.102:17177
-addnode=45.63.111.17:17177
-addnode=104.238.137.11:17177
-addnode=140.82.35.120:17177
-addnode=104.156.231.73:17177
-addnode=45.32.132.121:17177
-addnode=45.32.132.201:17177
-addnode=144.202.101.179:17177
-addnode=144.202.90.252:17177
-addnode=45.76.241.191:17177
-addnode=144.202.91.240:17177
-addnode=45.76.244.72:17177
-addnode=144.202.81.254:17177
-addnode=45.63.37.72:17177
-addnode=104.238.153.185:17177
-addnode=45.77.214.85:17177
-addnode=45.77.208.101:17177
-addnode=144.202.87.162:17177
-addnode=217.163.23.170:17177
+addnode=144.202.31.248:17172
+addnode=45.32.222.50:17172
+addnode=144.202.75.165:17172
+addnode=95.179.199.60:17172
+addnode=149.28.247.127:17172
+addnode=209.222.30.206:17172
+addnode=66.42.84.233:17172
 EOL
 }
 
@@ -164,21 +153,22 @@ EOL
 printf "\n\nSetting up your VPS now, this will take some time." > /dev/tty1
 printf "\nOnce finished a GUI login will greet you, press F3 for onboard keyboard\n" > /dev/tty1
 printf "\nCreating SWAP drive if needed: " > /dev/tty1
-prepare_swap
+prepareSwap
 
 printf "\nUpdating system!" > /dev/tty1
-update_system
+updateSystem
 
 printf "\nInstalling prerequisits!" > /dev/tty1
-install_prerequisits
+installPrerequisits
 
 printf "\nCreating XORG/LIGHTDM config!" > /dev/tty1
-xorg_config
-lightdm_config
+xorgConfig
+lightdmConfig
 
 printf "\nPreparing magnet related content!" > /dev/tty1
-download_magnet_wallet
-config_magnet_wallet
+downloadMagnetWallet
+configMagnetWallet
+createMagnetDesktopIcon
 
 printf "\nSetting console to GUI!" > /dev/tty1
 systemctl start lightdm
